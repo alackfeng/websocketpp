edf9041d75af6a1aa7db58140043faf265b3f6e3
fengy <fengy@cume.cc>
2019-07-30 11:44:41 +0800

    ssl auth

--- plugins/websocket_client/websocket_client.cpp
+++ plugins/websocket_client/websocket_client.cpp
@@ -237,8 +237,75 @@ void websocket_client_tls::init_tls(const std::string& hostname) {
 }
 
 
+/// Verify that one of the subject alternative names matches the given hostname
+bool verify_subject_alternative_name(const char * hostname, X509 * cert) {
+  STACK_OF(GENERAL_NAME) * san_names = NULL;
+
+  san_names = (STACK_OF(GENERAL_NAME) *) X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
+  if (san_names == NULL) {
+    return false;
+  }
+
+  int san_names_count = sk_GENERAL_NAME_num(san_names);
+
+  bool result = false;
+
+  for (int i = 0; i < san_names_count; i++) {
+    const GENERAL_NAME * current_name = sk_GENERAL_NAME_value(san_names, i);
+
+    if (current_name->type != GEN_DNS) {
+      continue;
+    }
+
+    char * dns_name = (char *) ASN1_STRING_data(current_name->d.dNSName);
+
+    // Make sure there isn't an embedded NUL character in the DNS name
+    if (ASN1_STRING_length(current_name->d.dNSName) != strlen(dns_name)) {
+      break;
+    }
+    // Compare expected hostname with the CN
+    result = (strcasecmp(hostname, dns_name) == 0);
+  }
+
+  sk_GENERAL_NAME_pop_free(san_names, GENERAL_NAME_free);
+
+  return result;
+
+}
+bool verify_common_name(const char * hostname, X509 * cert) {
+  int common_name_loc = X509_NAME_get_index_by_NID(X509_get_subject_name(cert), NID_commonName, -1);
+  if (common_name_loc < 0) {
+    return false;
+  }
+  X509_NAME_ENTRY * common_name_entry = X509_NAME_get_entry(X509_get_subject_name(cert), common_name_loc);
+  if (common_name_entry == NULL) {
+    return false;
+  }
+  ASN1_STRING * common_name_asn1 = X509_NAME_ENTRY_get_data(common_name_entry);
+  if (common_name_asn1 == NULL) {
+    return false;
+  }
+  char * common_name_str = (char *) ASN1_STRING_data(common_name_asn1);
+  if (ASN1_STRING_length(common_name_asn1) != strlen(common_name_str)) {
+    return false;
+  }
+  return (strcasecmp(hostname, common_name_str) == 0);
+}
 bool verify_certificate(const char * hostname, bool preverified, boost::asio::ssl::verify_context& ctx) {
-  std::cout << "verify_certificate ok" << std::endl;
+  std::cout << "verify_certificate " << "hostname: " << hostname << ", preverified: " << preverified << std::endl;
+
+  int depth = X509_STORE_CTX_get_error_depth(ctx.native_handle());
+  if (depth == 0 && preverified) {
+    X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
+    if (verify_subject_alternative_name(hostname, cert)) {
+      return true;
+    } else if (verify_common_name(hostname, cert)) {
+      return true;
+    } else {
+      std::cout << "verify_certificate verify failed, " << false << std::endl;
+      return true;
+    }
+  }
   return true;
 }
 
